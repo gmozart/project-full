142b7adbc4db9bc5f184a9ff2a59195277c8282a
gmozart <mozart.gleison@gmail.com>
2022-10-05 16:17:31 -0300

    Inicializando projeto Spring Secutiry

---
+++ src/main/java/dio/projectfull/ProjectFullApplication.java
@@ -0,0 +1,13 @@
+package dio.projectfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/projectfull/ProjectFullApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.projectfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
